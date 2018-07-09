69ce3eca84ff2adca4342ee3b1a3cde6dbf92d27
ahmetberkaycalisti <ahmetberkaycalisti@gmail.com>
2018-07-09 10:48:38 +0800

    updated

--- JSON_URLSESSION_GET_POST_REQUEST/ViewController.swift
+++ JSON_URLSESSION_GET_POST_REQUEST/ViewController.swift
@@ -2,9 +2,33 @@
 //  ViewController.swift
 //  JSON_URLSESSION_GET_POST_REQUEST
 //
-//  Created by Ahmet Berkay CALISTI on 09/07/2018.
-//  Copyright © 2018 Ahmet Berkay CALISTI. All rights reserved.
+//  Created by Ahmet CALISTI 陈星辉
+//  Copyright (c) 2014-2018 Ahmet CALISTI 陈星辉. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//
+//  Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//
+//  Neither the name of Ahmet CALISTI 陈星辉 nor the names of its contributors
+//  may be used to endorse or promote products derived from this software
+//  without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER ''AS IS'' AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import UIKit
 
@@ -20,6 +44,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onGetTapped(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            
+            if let response = response {
+                print(response)
+            }
+            if let data = data {
+                //                print(data)
+                
+                do {
+                    // we dont have to put anything. We can put mutable containers that just lets us modify some of the actual JSON that came in
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+                
+            }
+            
+            }.resume() //end of session.dataTask -- if you dont call "resume", it will not call this task. It'll just go through and nothing will happen 
+    }   // end of onGetTapped
+    
+    @IBAction func onPostTapped(_ sender: Any) {
+        
+        // Usually when you do a post request you have to pass in some parameters.
+        let parameters = ["username":"@ahmetberkaycalisti", "tweet":"Hello Bitchiz"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" // That will let thr urlsession knows that we are doing a post request
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type") // The application json is going to be set for the content type
+        
+        // JSONSerialization data is a throwing function so I have to do is market with try and I'm gonna use "guard" because I'm gonna do another do try block. so i'm just going to do a guard right here
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {return}
+        
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            }.resume()      // end of session.dataTask
+    }  // end of onPostTapepd
+    
+    
 
-}
+} // end of class ViewController
 
